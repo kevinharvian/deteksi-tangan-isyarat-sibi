b82c67e51df2acf1b4b410a95d6415e7eab1f08a
kevinharvian <kevinharvian@gmail.com>
2025-07-12 12:54:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode, ClientSettings
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
 import av
 import pickle
 import mediapipe as mp
